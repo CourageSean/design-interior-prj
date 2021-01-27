95c8b06631624aa2e6fb1f49a1517575a4a3f09b
CourageSean <35892827+CourageSean@users.noreply.github.com>
2021-01-27 15:53:29 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let massage = document.getElementById("massage");
+
+function submitFn() {
+  console.log(name.value);
+  console.log(email.value);
+  console.log(massage.value);
+}
